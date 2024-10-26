9c1cfd7e9c4bcf91c2c3111af767eee14af66ec3
koonukaame <marialezhebokova@yandex.ru>
2024-10-26 16:16:09 +0300

    feat: solve getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -261,8 +261,12 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const arr = [0, 1];
+  for (let i = 0; i <= index; i += 1) {
+    arr.push(arr[i] + arr[i + 1]);
+  }
+  return arr[index];
 }
 
 /**
