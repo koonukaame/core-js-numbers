5c6a874c629188c2c78e2491534d7ae36972002c
koonukaame <marialezhebokova@yandex.ru>
2024-10-26 16:00:02 +0300

    feat: solve toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -222,8 +222,15 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  if (
+    (typeof num === 'number' || num instanceof Number) &&
+    Number.isNaN(num) === false
+  ) {
+    return num;
+  }
+  return def;
 }
 
 /**
